2f0678387667eb6b1701f950fe7acc278729ae8b
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2026-02-25 01:17:46 -0300

    Add icon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::process::{Command, exit};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.is_empty() {
+        eprintln!("Cannot open links if no arguments were provided");
+        exit(1);
+    }
+
+    let script_path = r#"D:\Geral\Scripts\firefox_router.ps1"#;
+
+    let mut command = Command::new("powershell.exe");
+
+    command.arg("-ExecutionPolicy").arg("Bypass").arg("-File").arg(script_path);
+
+    for arg in &args {
+        command.arg(arg);
+    }
+
+    let status = match command.status() {
+        Ok(status) => status,
+        Err(_) => {
+            eprintln!("An error occurred");
+            exit(1);
+        }
+    };
+
+    if !status.success() {
+        exit(status.code().unwrap_or(1));
+    }
 }
